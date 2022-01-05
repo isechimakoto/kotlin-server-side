581c0b753f2cb31f82996f605e04bce5edd66f9b
Makoto Isechi <misechi@callmehappening.com>
2022-01-06 00:34:17 +0900

    Add setting of database

--- src/main/kotlin/com/example/kotlinserverside/entity/Item.kt
+++ src/main/kotlin/com/example/kotlinserverside/entity/Item.kt
@@ -3,6 +3,14 @@ package com.example.kotlinserverside.entity
 import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
 
+/**
+ * Data class of the items table that assumed structure below.
+ * create table items (
+ *   id    INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+ *   name  varchar(32)            not null,
+ *   price integer                not null
+ * );
+ */
 @Table("items")
 data class Item(
     @Id val id: Int,
